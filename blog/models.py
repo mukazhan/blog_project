76fafaf88cf239a8f986747aacaef6d9eced675e
mukazhan <mukazhanovn@gmail.com>
2024-01-22 09:58:18 +0600

    second commit

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, )
     body = models.TextField()
+    add_info = models.TextField()
 
     def __str__(self):
         return self.title
